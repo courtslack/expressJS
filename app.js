54e3fb244aeaffdafb4d37582d0d2092e12036cd
Courtney Slack <200154182@fvtc.edu>
2026-04-03 21:47:47 -0500

    exercise 1

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express');
+//const { request } = require('http');
 const app = express();
 const port = 3000;
 const root = path.join(__dirname, 'public')
@@ -26,7 +27,11 @@ app.get('/', (request, response) => {
     response.sendFile('index.html', { root })
 })
 
-app.get('/pokemon/???')
+app.get('/pokemon/:id', (request, response) => {
+    response.sendFile('index.html', { root })
+})
+
+//app.get('/pokemon/???')
 
 app.get('/api/v1/random-pokemon', (request, response) => {
     const randomNmber = Math.floor(Math.random() * 9) 
@@ -42,5 +47,18 @@ app.get('/api/v1/pokemon/:id', (request, response) => {
 })
 
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
+    console.log(`http://localhost:${port}/`);
+});
+
+//exercise one
+app.get ('/type/:type', (request, response) => {
+    response.sendFile('index.html', {root})
+})
+
+app.get('/api/v1/random-pokemon/:type', (request, response) => {
+    const { type } = request.params
+    const found = pokemon.filter(p => p.type.toLocaleLowerCase() === type.toLowerCase())
+    const r = Math.floor(Math.random() * found.length)
+    if (found.length > 0 ) response.send(found[r])
+    else response.send({ error: { message: `Could not find pokemon with type: ${type}` }})
 });
