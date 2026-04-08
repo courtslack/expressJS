7e2581fccad41702ebbb9b46b3c7cf976049fc54
Courtney Slack <200154182@fvtc.edu>
2026-04-08 13:05:57 -0500

    working on id

--- routes/api/v1/pokemon.js
+++ routes/api/v1/pokemon.js
@@ -1,6 +1,63 @@
 const router = require('express').Router()
 const { getCollection, ObjectId } = require('../../../dbconnect')
 
+let collection = null  //If I don’t have the collection, go get it. If I already have it, just use it.
+const getPokemon = async () => {
+    if (!collection) collection = await getCollection('PokemonAPI', 'Pokemon')
+    return collection
+}
+
+router.get('/byId/:id', async (request, response) => {
+    const { id } = request.params
+    const collection = await getPokemon()
+    const found = await collection.findOne({ _id: new ObjectId(id) })
+    if (found) response.send(found)
+    else response.send({ error: { message: `Could not find pokemon with id: ${id}` }})
+})
+
+router.get('/random', async (request, response) => {  
+    const collection = await getPokemon() 
+    const count = await collection.countDocuments()
+    const number = Math.floor(Math.random() * count) + 1   
+    const found = await collection.findOne({ "number": parseInt(number) })
+    if (found) response.send(found)
+    else response.send({ error: { message: `Could not find pokemon with number: ${number}` }})
+})
+
+router.post('/add', async (request, response) => {
+    const { number, name, type } = request.body
+    const collection = await getPokemon()
+    const { acknowledged, insertedId } = await collection.insertOne({ number, name, type })
+    response.send({ acknowledged, insertedId })
+})
+
+router.get('/:number', async (request, response) => {
+    const { number } = request.params
+   // const collection = await getCollection('PokemonAPI', 'Pokemon')
+    const collection = await getPokemon()
+    const found = await collection.findOne({ "number": parseInt(number) })
+    if (found) response.send(found)
+    else response.send({ error: { message: `Could not find pokemon with number: ${number}` }})
+})
+
+router.get('/random/:type', async (request, response) => {
+    const { type } = request.params
+    //const collection = await getCollection('PokemonAPI', 'Pokemon')
+    const collection = await getPokemon()
+    const foundOfType = await collection.find({ "type": type }).toArray() //get all of type
+    const count = foundOfType.length
+    if (count === 0) response.send({ error: { message: `Could not find pokemon with type: ${type}` }})
+    const number = Math.floor(Math.random() * count)// + 1 <-- error in the video
+    console.log(number, foundOfType)
+    response.send(foundOfType[number])
+
+    // console.log(foundOfType);
+    // response.send('done')
+})
+
+module.exports = router
+
+
 // const pokemon = [
 //     { id: 1, name: 'Bulbasaur', type: 'Grass' },
 //     { id: 2, name: 'Ivysaur', type: 'Grass' },
@@ -12,53 +69,3 @@ const { getCollection, ObjectId } = require('../../../dbconnect')
 //     { id: 8, name: 'Wartortle', type: 'Water' },
 //     { id: 9, name: 'Blastoise', type: 'Water' },
 // ]
-
-// router.get('/random', (request, response) => {
-//     const r = Math.floor(Math.random() * 9)
-//     response.send(pokemon[r])
-// })
-router.get('/random', async (_, response) => {
-    const collection = await getCollection('PokemonAPI', 'Pokemon')
-    const count = await collection.countDocuments()
-    const number = Math.floor(Math.random() * count) + 1
-    const found = await collection.findOne({ "number": parseInt(number) })
-    if (found) response.send(found)
-    else response.send({ error: { message: `Could not find pokemon with number: ${number}` }})
-})
-
-router.post('/add', (request, response) => {
-    const { id, name, type } = request.body
-    console.log({ id, name, type })
-
-    const found = pokemon.find(p => p.id.toString() === id.toString())
-    if (found) response.send({ error: { message: `Pokemon with id: ${id}, already exists`} })
-    else pokemon.push({ id, name, type })
-})
-
-// router.get('/:id', (request, response) => {
-//     const { id } = request.params
-//     const found = pokemon.find(p => p.id.toString() === id)
-//     if (found) response.send(found)
-//     else response.send({ error: { message: `Could not find pokemon with id: ${id}` }})
-// })
-
-router.get('/:number', async (request, response) => {
-    const { number } = request.params
-    const collection = await getCollection('PokemonAPI', 'Pokemon')
-    const found = await collection.findOne({ "number": parseInt(number) })
-    if (found) response.send(found)
-    else response.send({ error: { message: `Could not find pokemon with number: ${number}` }})
-})
-
-router.get('/random/:type', async (request, response) => {
-    const { type } = request.params
-    const collection = await getCollection('PokemonAPI', 'Pokemon')
-    const foundOfType = await collection.find({ "type": type }).toArray()
-    const count = foundOfType.length
-    if (count === 0) response.send({ error: { message: `Could not find pokemon with type: ${type}` }})
-    const number = Math.floor(Math.random() * count)// + 1 <-- error in the video
-    //console.log(number, foundOfType)
-    response.send(foundOfType[number])
-})
-
-module.exports = router
